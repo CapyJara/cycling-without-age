a2665a545147eadaad46c99c4d300200313c1cea
Jared Myhrberg <jared.myhrberg@gmail.com>
2019-02-14 08:43:17 -0800

    tried putting new stors into a fieldset, not makig new fieldset, just adding to the story and title sections

--- src/story.js
+++ src/story.js
@@ -1,28 +1,25 @@
 const storyButton = document.getElementById('new-story-button');
-const newStoryP = document.getElementById('new-story');
 
 storyButton.addEventListener('click', function(event) {
     event.preventDefault();
 
-    // const newStorySection = document.getElementById('new-story-section');
-
-    // creating new p element to place story input into
-    const newStoryInput = document.getElementById('new-story-input');
-
-    const newStory = document.createElement('p');
-    newStory.textContent = newStoryInput.value;
-
-    newStoryP.appendChild(newStory);
+    // section where input title and story are stored
+    const newStorySection = document.getElementById('new-story-display');
 
     // creating new story title
     const newStoryTitleInput = document.getElementById('new-story-title-input');
-
-    const newTitle = document.createElement('a');
+    const titleLegend = document.getElementById('new-story-title');
+    const newTitle = document.createElement('legend');
     newTitle.textContent = newStoryTitleInput.value;
-    // newTitle.href = 
-        // dynamically create new entry
+   
+    titleLegend.appendChild(newTitle);
+    newStorySection.appendChild(titleLegend);
 
+    // creating new p element to place story input into
+    const newStoryInput = document.getElementById('new-story-input');
+    const newStory = document.createElement('p');
+    newStory.textContent = newStoryInput.value;
 
-    console.log(newStoryInput.value);
+    newStorySection.appendChild(newStory);
     
 }); 
