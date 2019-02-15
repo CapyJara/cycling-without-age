b23a13fb10ec081f51b018412b2acb089e8bbb3a
Jared Myhrberg <jared.myhrberg@gmail.com>
2019-02-15 08:50:15 -0800

    didnt do much else

--- src/story.js
+++ src/story.js
@@ -1,25 +1,34 @@
-const storyButton = document.getElementById('new-story-button');
+const formStoryInput = document.getElementById('form-story');
 
-storyButton.addEventListener('click', function(event) {
+formStoryInput.addEventListener('submit', function(event) {
     event.preventDefault();
 
+    const formData = new FormData(formStoryInput);
     // section where input title and story are stored
+
+    const story = {
+        title: formData.get('title-input'),
+        story: formData.get('story-input')
+    };
+
     const newStorySection = document.getElementById('new-story-display');
-
+    
     // creating new story title
-    const newStoryTitleInput = document.getElementById('new-story-title-input');
-    const titleLegend = document.getElementById('new-story-title');
-    const newTitle = document.createElement('legend');
-    newTitle.textContent = newStoryTitleInput.value;
+    const newTitle = story.title;
+    console.log(story.title);
+
+    newStorySection.appendChild(newTitle);
+    
+    //newTitle.textContent = newStoryTitleInput.value;
    
-    titleLegend.appendChild(newTitle);
-    newStorySection.appendChild(titleLegend);
+    // titleLegend.appendChild(newTitle);
+    // newStorySection.appendChild(titleLegend);
 
-    // creating new p element to place story input into
-    const newStoryInput = document.getElementById('new-story-input');
-    const newStory = document.createElement('p');
-    newStory.textContent = newStoryInput.value;
+    // // creating new p element to place story input into
+    // const newStoryInput = document.getElementById('new-story-input');
+    // const newStory = document.createElement('p');
+    // newStory.textContent = newStoryInput.value;
 
-    newStorySection.appendChild(newStory);
+    // newStorySection.appendChild(newStory);
     
 }); 
